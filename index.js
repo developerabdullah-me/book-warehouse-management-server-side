b374cef02f6343642ffeb5324e94e973ecebe185
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-05-02 04:59:07 +0600

    Post data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port= process.env.PORT || 5000;
 
 // middellware 
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.manly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -23,6 +24,13 @@ try{
     const cursor=ServiceCollection.find(query)
     const services=await cursor.toArray()
     res.send(services)
+    })
+
+    app.post('/services', async(req, res) => {
+      
+        const query=req.body;
+        const service = await ServiceCollection.insertOne(query).toArray()
+        res.send(service)
     })
 }
 finally{
