2de0aa36d8c7176a418ed496f76335028e371993
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-05-01 21:22:02 +0600

    get data from mongodb

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+require("dotenv").config();
 const port= process.env.PORT || 5000;
 
 // middellware 
@@ -9,7 +9,27 @@ app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.manly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run (){
+try{
+    await client.connect();
+    const ServiceCollection=client.db('warehouse').collection('service')
+
+    app.get('/services',async (req, res) => {
+        const query={}
+    const cursor=ServiceCollection.find(query)
+    const services=await cursor.toArray()
+    res.send(services)
+    })
+}
+finally{
+
+}
+    }
+    run().catch(console.dir)
 app.get('/', (req, res) =>{
     res.send('welcome warehouse-server-site')
 })
