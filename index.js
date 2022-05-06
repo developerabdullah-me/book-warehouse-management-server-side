55ff8c6ffee9c06b4c4667e07ed06700203b9988
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-05-06 12:02:56 +0600

    myAddedItems filter

--- index.js
+++ index.js
@@ -1,61 +1,74 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port= process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
 
-// middellware 
+// middellware
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.manly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run (){
-try{
+async function run() {
+  try {
     await client.connect();
-    const ServiceCollection=client.db('warehouse').collection('service')
+    const ServiceCollection = client.db("warehouse").collection("service");
 
-    app.get('/InventoryItems',async (req, res) => {
-        const query={}
-    const cursor=ServiceCollection.find(query)
-    const InventoryItems=await cursor.toArray()
-    res.send(InventoryItems)
-    })
-// post
-    app.post('/InventoryItems', async(req, res) => {
-        const newService=req.body;
-        const service = await ServiceCollection.insertOne(newService)
-        res.send(service)
-    })
+    app.get("/InventoryItems", async (req, res) => {
+      const query = {};
+      const cursor = ServiceCollection.find(query);
+      const InventoryItems = await cursor.toArray();
+      res.send(InventoryItems);
+    });
+    // get myAddedItems data  from database
+    app.get("/myAddedItems", async (req, res) => {
+      const email=req.query.email;
+      console.log(email);
+      const query = {email: email};
+      const cursor = ServiceCollection.find(query);
+      const InventoryItems = await cursor.toArray();
+      res.send(InventoryItems);
+    });
+    // post
+    app.post("/myAddedItems", async (req, res) => {
+      const newService = req.body;
+      const service = await ServiceCollection.insertOne(newService);
+      res.send(service);
+    });
+    // post
+    app.post("/InventoryItems", async (req, res) => {
+      const newService = req.body;
+      const service = await ServiceCollection.insertOne(newService);
+      res.send(service);
+    });
 
-    app.get('/InventoryItems/:id', async (req, res) => {
-        const id = req.params.id
-        const query = { _id: ObjectId(id) }
-        const result = await ServiceCollection.deleteOne(query)
-        res.send(result)
-    })
+    app.get("/InventoryItems/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ServiceCollection.deleteOne(query);
+      res.send(result);
+    });
 
-
-    app.delete('/InventoryItems/:id', async (req, res) => {
-        const id = req.params.id
-        const query = { _id: ObjectId(id) }
-        const result = await ServiceCollection.deleteOne(query)
-        res.send(result)
-    })
-
+    app.delete("/InventoryItems/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ServiceCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
 }
-finally{
-
-}
-    }
-    run().catch(console.dir)
-app.get('/', (req, res) =>{
-    res.send('welcome warehouse-server-site')
-})
-app.listen(port,()=>{
-console.log('listening on',port);
-})
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("welcome warehouse-server-site");
+});
+app.listen(port, () => {
+  console.log("listening on", port);
+});
