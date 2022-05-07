5c108d96526946dbf9f75fd0293988c796e17493
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-05-07 23:46:15 +0600

    rSolving

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const ObjectId = require('mongodb').ObjectId
-const { MongoClient, ServerApiVersion} = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
-
 // middellware
 app.use(cors());
 app.use(express.json());
-
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-      return res.status(401).send({ message: 'unauthorized access' });
+    return res.status(401).send({ message: "unauthorized access" });
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-          return res.status(403).send({ message: 'Forbidden access' });
-      }
-      console.log('decoded', decoded);
-      req.decoded = decoded;
-      next();
-  })
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
 }
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.manly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -41,14 +37,13 @@ async function run() {
     await client.connect();
     const ServiceCollection = client.db("warehouse").collection("service");
 
-    app.post('/login', async (req, res) => {
+    app.post("/login", async (req, res) => {
       const user = req.body;
       const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-          expiresIn: '1d'
+        expiresIn: "1d",
       });
       res.send({ accessToken });
-  })
-
+    });
 
     app.get("/InventoryItems", async (req, res) => {
       const query = {};
@@ -58,19 +53,17 @@ async function run() {
     });
     // get myAddedItems data  from database
 
-    app.get("/myAddedItems",verifyJWT, async (req, res) => {
+    app.get("/myAddedItems", verifyJWT, async (req, res) => {
       const decodedEmail = req.decoded.email;
-      const email=req.query.email;
-      if (email === decodedEmail){
-        const query = {email: email};
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
         const cursor = ServiceCollection.find(query);
         const InventoryItems = await cursor.toArray();
         res.send(InventoryItems);
+      } else {
+        res.status(403).send({ message: "Access denied! Forbidden access" });
       }
-      else{
-        res.status(403).send({ message: 'Access denied! Forbidden access' })
-   }
-     
     });
     // post
     app.post("/myAddedItems", async (req, res) => {
@@ -79,16 +72,16 @@ async function run() {
       res.send(service);
     });
 
-// id
-    app.get('/InventoryItems/:id', async (req, res) => {
+    // id
+    app.get("/InventoryItems/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
-      const query = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       console.log(query);
-      const result = await ServiceCollection.findOne(query)
-      console.log(result)
+      const result = await ServiceCollection.findOne(query);
+      console.log(result);
       res.send(result);
-  });
+    });
     // post
     app.post("/InventoryItems", async (req, res) => {
       const newService = req.body;
@@ -98,7 +91,7 @@ async function run() {
 
     app.get("/InventoryItems/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id:id };
+      const query = { _id: id };
       const result = await ServiceCollection.deleteOne(query);
       res.send(result);
     });
